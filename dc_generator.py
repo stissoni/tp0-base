341a18d11f7848a67864bad3ae8d85f19468d03e
Santiago Tissoni <stissoni@santandertecnologia.com.ar>
2023-04-06 11:02:37 -0300

    1,1

--- dc_generator.py
+++ dc_generator.py
@@ -1,37 +1,39 @@
 import yaml
+import sys
 
-N_CLIENTS = 3
+if __name__ == "__main__":
 
-my_dict = {
-    "networks": {
-        "testing_net": {
-            "ipam": {"driver": "default", "config": [{"subnet": "172.25.125.0/24"}]}
-        }
-    },
-    "services": {
-        "server": {
-            "container_name": "server",
-            "image": "server:latest",
-            "entrypoint": "python3 /main.py",
-            "environment": ["PYTHONUNBUFFERED=1", "LOGGING_LEVEL=DEBUG"],
-            "networks": ["testing_net"],
-        }
-    },
-    "version": "3.9",
-}
+    N_CLIENTS = int(sys.argv[1])
 
-for i in range(N_CLIENTS):
-    client = f"client{i+1}"
-
-    my_dict["services"][client] = {
-        "container_name": f"{client}",
-        "image": "client:latest",
-        "entrypoint": "/client",
-        "environment": [f"CLI_ID={i+1}", "LOGGING_LEVEL=DEBUG"],
-        "networks": ["testing_net"],
-        "depends_on": ["server"],
+    my_dict = {
+        "networks": {
+            "testing_net": {
+                "ipam": {"driver": "default", "config": [{"subnet": "172.25.125.0/24"}]}
+            }
+        },
+        "services": {
+            "server": {
+                "container_name": "server",
+                "image": "server:latest",
+                "entrypoint": "python3 /main.py",
+                "environment": ["PYTHONUNBUFFERED=1", "LOGGING_LEVEL=DEBUG"],
+                "networks": ["testing_net"],
+            }
+        },
+        "version": "3.9",
     }
 
+    for i in range(N_CLIENTS):
+        client = f"client{i+1}"
 
-with open("docker-compose-dev.yaml", "w") as f:
-    yaml.dump(my_dict, f)
+        my_dict["services"][client] = {
+            "container_name": f"{client}",
+            "image": "client:latest",
+            "entrypoint": "/client",
+            "environment": [f"CLI_ID={i+1}", "LOGGING_LEVEL=DEBUG"],
+            "networks": ["testing_net"],
+            "depends_on": ["server"],
+        }
+
+    with open("docker-compose-dev.yaml", "w") as f:
+        yaml.dump(my_dict, f)
